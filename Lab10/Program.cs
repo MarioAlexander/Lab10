8aeb3ce72cf7c6d1a463e1c1e51397fefde95f46
Alexander SB <alexsb12348@gmail.com>
2017-11-20 23:47:24 -0500

    Lab 10 - Github

---
+++ Lab10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwndDataContext context = new NorthwndDataContext();
+
+            //Creacion consulta
+            var query = from p in context.Products
+                        select p;
+
+            //Ejecutccion consulta 
+            foreach (var prod in query)
+            {
+                Console.WriteLine("ID={0} \t Name={1}", prod.ProductID, prod.ProductName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
