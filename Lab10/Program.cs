12ebdd634ba4d1e84025709f6e36dad0defc5c79
Alexander SB <alexsb12348@gmail.com>
2017-11-25 14:31:18 -0500

    lab10 terminado

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,55 @@ namespace Lab10
         {
             NorthwndDataContext context = new NorthwndDataContext();
 
-            //Creacion consulta
-            var query = from p in context.Products
+            var query = from p in context.Suppliers
+                        where SqlMethods.Like(p.Country, "%USA%")
                         select p;
-
-            //Ejecutccion consulta 
-            foreach (var prod in query)
+            foreach (var supp in query)
             {
-                Console.WriteLine("ID={0} \t Name={1}", prod.ProductID, prod.ProductName);
+                Console.WriteLine("ID={0} \t Name={1} \t Country={2}", supp.SupplierID, supp.CompanyName, supp.Country);
             }
-            Console.ReadKey();
+            Console.ReadKey(); 
+
+            /*
+
+            var query = from q in context.Products 
+                        from p in context.Suppliers
+                        where q.SupplierID == p.SupplierID
+                         select new { ProductID = q.ProductID, ProductName = q.ProductName, CompanyName = p.CompanyName };
+            foreach (var product in query)
+            {
+                    Console.WriteLine("ID={0} \t Name={1} \t Supplier={2}", product.ProductID, product.ProductName, product.CompanyName);
+            }
+
+            Console.ReadKey();*/
+            /*var product = (from p in context.Products
+                           where p.ProductID == 78
+                           select p).FirstOrDefault();
+
+            context.Products.DeleteOnSubmit(product);
+            context.SubmitChanges();
+            */
+
+            /*
+            Categories p = new Categories();
+            p.CategoryName = "Hamburguers";
+            p.Description = "Hamburguesas de carne, pollo, pavo, etc";
+
+
+            context.Categories.InsertOnSubmit(p);
+            context.SubmitChanges();
+            */
+            //Creacion consulta
+            /*  var query = from p in context.Products
+                          where SqlMethods.Like(p.ProductName, "A%")
+                          select p;
+
+              //Ejecutccion consulta 
+              foreach (var prod in query)
+              {
+                  Console.WriteLine("ID={0} \t Name={1} \t Price={2}", prod.ProductID, prod.ProductName, prod.UnitPrice);
+              }
+              Console.ReadKey();*/
         }
     }
 }
